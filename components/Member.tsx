8e57d3af07cf054ae6a10ccf55048dc5e0aa7479
yechoi42 <yechoi@student.42seoul.kr>
2022-02-23 23:51:53 +0900

    feat: add Member component

---
+++ components/Member.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image'
+type Props = {
+    name: string
+    role: string
+    gitId: string
+    imgLocation: string
+}
+
+export default function Member({name, role, gitId, imgLocation}: Props){
+    return (
+        <>
+        <div className="member-container">
+            <div className="member-img-wrapper">
+            <Image src={imgLocation} alt={gitId} width="100%" height="100%" objectFit='cover'/>
+            </div>
+            <div className="name">{name}</div>
+            <div className="role">{role}</div>
+        </div>
+        <style jsx>{`
+            .member-container{
+                cursor: pointer;
+                position: relative;
+                display: block;
+                margin-left: 1rem;
+                margin-right: 1rem;
+                margin-bottom: 45px;
+                overflow: hidden;
+                text-align: center;
+            }
+            .member-img-wrapper{
+                max-width: 9rem;
+                border-radius: 50%;
+                overflow: hidden;
+            }
+            .name {
+                font-size: 1.5rem;
+            }
+        `}</style>
+        </>
+    )
+}
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,15 +1,29 @@
 import type { NextPage } from 'next'
+import Member from '../components/Member'
 import styles from '../styles/Home.module.css'
 import HeadComponent from '../components/HeadComponent'
 
 const About: NextPage = () => {
   return (
+    <>
     <div className={styles.container}>
       <HeadComponent title="About" />
       <main className={styles.main}>
         <h1 className={styles.title}>About</h1>
       </main>
+      <div className="members-container">
+      <Member name="yeji" role="interviewer" gitId="yechoi42" imgLocation="/yechoi.jpeg" />
+      </div>
     </div>
+    <style jsx>{`
+      .members-container {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+      }
+    `}</style>
+    </>
   )
 }
 
