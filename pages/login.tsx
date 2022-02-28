b1e894cb7fdf02129a02cbc1855fd0842a10a5a5
yechoi42 <yechoi@student.42seoul.kr>
2022-02-28 22:33:21 +0900

    chore: lint & gitignore

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,38 +1,50 @@
-import { NextPage } from "next"
+import { NextPage } from 'next'
 
 const Login: NextPage = () => {
-    return (
-        <>
-        <div className="container login-container">
-            <button type="button" className="btn btn-primary">42 API로 로그인하기</button>
-            <p>OR</p>
-            <div className="form-group">
-                <div className="form-floating mb-3">
-                    <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
-                    <label htmlFor="floatingInput">Email address</label>
-                </div>
-                <div className="form-floating">
-                    <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
-                    <label htmlFor="floatingPassword">Password</label>
-                </div>
-            </div>
+  return (
+    <>
+      <div className="container login-container">
+        <button type="button" className="btn btn-primary">
+          42 API로 로그인하기
+        </button>
+        <p>OR</p>
+        <div className="form-group">
+          <div className="form-floating mb-3">
+            <input
+              type="email"
+              className="form-control"
+              id="floatingInput"
+              placeholder="name@example.com"
+            />
+            <label htmlFor="floatingInput">Email address</label>
+          </div>
+          <div className="form-floating">
+            <input
+              type="password"
+              className="form-control"
+              id="floatingPassword"
+              placeholder="Password"
+            />
+            <label htmlFor="floatingPassword">Password</label>
+          </div>
         </div>
-        <style jsx>{`
-            .login-container {
-                text-align: center;
-                align-items: center;
-                max-width: 800px;
-            }
-            .form-group {
-                display: table;
-                margin: auto;
-            }
-            .form-floating {
-                align-items: center;
-            }
-        `}</style>
-        </>
-    )
+      </div>
+      <style jsx>{`
+        .login-container {
+          text-align: center;
+          align-items: center;
+          max-width: 800px;
+        }
+        .form-group {
+          display: table;
+          margin: auto;
+        }
+        .form-floating {
+          align-items: center;
+        }
+      `}</style>
+    </>
+  )
 }
 
 export default Login
