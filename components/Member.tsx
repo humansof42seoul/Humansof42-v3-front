5f8f88899203c2f207f032b286732d681ea5466d
Yeji Choi <61399588+yechoi42@users.noreply.github.com>
2022-03-11 23:52:46 +0900

    feat: add member component at about page (#2)
    * feat: add Member component
    
    * chore: lint
    
    * feat: about page

---
+++ components/Member.tsx
@@ -0,0 +1,43 @@
+import Image from 'next/image'
+type Props = {
+  name: string
+  role: string
+  gitId: string
+  imgLocation: string
+}
+
+export default function Member({ name, role, gitId, imgLocation }: Props) {
+  return (
+    <>
+      <div className="member-container">
+        <div className="member-img-wrapper">
+          <Image src={imgLocation} alt={gitId} width="100%" height="100%" />
+        </div>
+        <div className="name">{name}</div>
+        <div className="role">{role}</div>
+      </div>
+      <style jsx>{`
+        .member-container {
+          cursor: pointer;
+          position: relative;
+          display: block;
+          margin-left: 1rem;
+          margin-right: 1rem;
+          margin-bottom: 45px;
+          overflow: hidden;
+          text-align: center;
+        }
+        .member-img-wrapper {
+          max-width: 9rem;
+          border-radius: 50%;
+          width: 100%;
+          position: relative;
+        }
+
+        .name {
+          font-size: 1.5rem;
+        }
+      `}</style>
+    </>
+  )
+}
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,15 +1,52 @@
 import type { NextPage } from 'next'
+import Member from '../components/Member'
 import styles from '../styles/Home.module.css'
 import HeadComponent from '../components/HeadComponent'
 
 const About: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <HeadComponent title="About" />
-      <main className={styles.main}>
-        <h1 className={styles.title}>About</h1>
-      </main>
-    </div>
+    <>
+      <div className={styles.container}>
+        <HeadComponent title="About" />
+        <main className={styles.main}>
+          <h1 className={styles.title}>About</h1>
+        </main>
+        <div className="members-container">
+          <Member
+            name="yeji"
+            role="interviewer"
+            gitId="yechoi42"
+            imgLocation="/yechoi.jpeg"
+          />
+          <Member
+            name="dog"
+            role="interviewer"
+            gitId="yechoi42"
+            imgLocation="/dog.jpeg"
+          />
+          <Member
+            name="yeji"
+            role="interviewer"
+            gitId="yechoi42"
+            imgLocation="/yechoi.jpeg"
+          />
+          <Member
+            name="yeji"
+            role="interviewer"
+            gitId="yechoi42"
+            imgLocation="/yechoi.jpeg"
+          />
+        </div>
+      </div>
+      <style jsx>{`
+        .members-container {
+          display: flex;
+          flex-direction: row;
+          flex-wrap: wrap;
+          justify-content: center;
+        }
+      `}</style>
+    </>
   )
 }
 
